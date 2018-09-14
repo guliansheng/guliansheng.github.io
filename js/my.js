2f33b546824adfa7b037485c67cd681a20ccc752
gu <1229303942@qq.com>
2018-09-14 15:43:07 +0800

    Site updated: 2018-09-14 15:42:32

--- js/my.js
+++ js/my.js
@@ -3,3 +3,14 @@ type your custom JavaScript here
 在本文件中键入您的自定义JS脚本
 升级主题文件时请注意保护本文件，防止意外覆盖。
 */
+// setTimeout(function(){
+//   newImg = new Image; //创建一个临时img标签,将真实图片的src先让这个临时标签检测是否为有效地址
+//   newImg.src = img.getAttribute('tureSrc');
+//   newImg.onload = function(){ //当图片正常加载时才会执行
+//       img.style.display = 'block';
+//       img.src = this.src;
+//       newImg = null;
+//       console.log("图片加载完成!");
+//   }
+//   console.log("图片加载中...");
+// },1000);
